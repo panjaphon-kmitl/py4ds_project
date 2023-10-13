3faebb46949151aac13a5709fa6da11e630824c1
FatCat4Real <93673067+FatCat4Real@users.noreply.github.com>
2023-10-14 00:59:19 +0700

    FIX : ex19

--- src/ex19.py
+++ src/ex19.py
@@ -11,6 +11,25 @@ SPECIAL = '~!@#$%^&*()_+'                       # 13
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
 
-def gen_password(param):
+def gen_password(length):
     # TODO : complete this
-    pass
+    min_length = 12
+    length = max(length, min_length)
+    pwd = ''
+    # required chars
+    chars_set = [LOWER_LETTERS, UPPER_LETTERS, NUMBERS, SPECIAL]
+    for i in chars_set:
+        pwd += random.choice(i)
+
+    # random so the output doesn't have prefix pattern
+    pwd = list(pwd)
+    random.shuffle(pwd)
+    pwd = ''.join(pwd)
+
+    # other chars
+    for i in range(length - len(chars_set)):
+        pwd += random.choice(ALL_CHARS)
+
+    return pwd
+
+
