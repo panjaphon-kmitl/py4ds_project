ac16662529e250a53af7347884a3e572799ee217
FatCat4Real <93673067+FatCat4Real@users.noreply.github.com>
2023-10-14 02:01:19 +0700

    FIX : ex22

--- src/ex22.py
+++ src/ex22.py
@@ -3,7 +3,7 @@ Exercise 22 : Rock Paper Scissor
 """
 
 
-def rps_check(param, param1):
+def rps_check(p1, p2):
     """
     Check the result of a rock-paper-scissors game between two players.
 
@@ -14,5 +14,17 @@ def rps_check(param, param1):
     Returns:
         str: The result of the game. It can be 'player one', 'player two', or 'tie'.
     """
-    # TODO : complete this
-    pass
+    # FIX : complete this
+    selected = [p1, p2]
+    map = {p1:'player one', p2:'player two'}
+
+    if p1 == p2:
+        return 'tie'
+    elif 'rock' in selected and 'paper' in selected:
+        return map['paper']
+    elif 'rock' in selected and 'scissors' in selected:
+        return map['rock']
+    elif 'paper' in selected and 'scissors' in selected:
+        return map['scissors']
+
+
