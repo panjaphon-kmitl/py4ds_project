b6a9b42140934a2cf534a67ddb8ecc02c7d9b05a
FatCat4Real <93673067+FatCat4Real@users.noreply.github.com>
2023-10-14 00:40:44 +0700

    FIX : ex18

--- src/ex18.py
+++ src/ex18.py
@@ -3,5 +3,10 @@ Exercise 18 : Buy 8 get 1 free
 """
 
 
-def get_cost_of_coffee(param, param1):
-    pass
+def get_cost_of_coffee(cups, price_each):
+    if cups == 0:
+        return 0
+    else:
+        free_cups = (cups-1) // 8
+        not_free_cups = cups - free_cups
+        return not_free_cups  * price_each
