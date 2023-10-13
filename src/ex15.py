7a3428b15cb57fcbdf4e884a6d33835ab0e373f0
FatCat4Real <93673067+FatCat4Real@users.noreply.github.com>
2023-10-14 00:13:02 +0700

    FIX : ex15

--- src/ex15.py
+++ src/ex15.py
@@ -3,7 +3,7 @@ Exercise 15
 """
 
 
-def median(params):
+def median(num_list):
     """
     Calculate the median of a list of numbers.
 
@@ -13,4 +13,15 @@ def median(params):
     Returns:
         [int, None]: The median value of the list, or None if the list is empty.
     """
-    pass
+    num_list = sorted(num_list)
+    if num_list == []:
+        return None
+    else:
+        length = len(num_list)
+        if length % 2 == 0:
+            end = int((length / 2))
+            start = end - 1
+            return (num_list[start] + num_list[end]) / 2
+        elif length % 2 == 1:
+            mid = int((length - 1) / 2)
+            return num_list[mid]
