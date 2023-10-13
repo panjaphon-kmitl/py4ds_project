fe2cfebbe2b47c1193bb4318980570418c55d93a
FatCat4Real <93673067+FatCat4Real@users.noreply.github.com>
2023-10-14 00:02:42 +0700

    FIX : ex13

--- src/ex13.py
+++ src/ex13.py
@@ -3,7 +3,7 @@ Exercise 13
 """
 
 
-def calc_sum(params):
+def calc_sum(num_list):
     """
     Calculate the sum of a list of numbers.
 
@@ -13,10 +13,10 @@ def calc_sum(params):
     Returns:
     - int: The sum of all the numbers in the list.
     """
-    pass
+    return 0 if num_list == [] else sum(num_list)
 
 
-def calc_prod(params):
+def calc_prod(num_list):
     """
     Calculates the product of all the numbers in the given list.
 
@@ -26,4 +26,10 @@ def calc_prod(params):
     Returns:
         int: The product of all the numbers in the list.
     """
-    pass
+    if num_list == []:
+        return 1
+    else:
+        total = 1
+        for i in num_list:
+            total *= i
+        return total
